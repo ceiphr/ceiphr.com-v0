2fc94dd652a3d33535a48f45d078ec5a237d25b8
Ari <ceiphr@pm.me>
2019-04-06 12:48:24 -0400

    Added MathJax properly

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,4 +3,15 @@
 $(document).ready(function () {
 	$('.sidenav').sidenav();
 	$('.tooltipped').tooltip();
+	MathJax.Hub.Config({
+		jax: ["input/TeX", "input/MathML", "input/AsciiMath", "output/CommonHTML"],
+		extensions: ["tex2jax.js", "mml2jax.js", "asciimath2jax.js", "MathMenu.js", "MathZoom.js", "AssistiveMML.js", "a11y/accessibility-menu.js"],
+		TeX: {
+			extensions: ["AMSmath.js", "AMSsymbols.js", "noErrors.js", "noUndefined.js"]
+		},
+		tex2jax: {
+			inlineMath: [['$', '$'], ['\(', '\)']],
+			processEscapes: true
+		},
+	});
 });
