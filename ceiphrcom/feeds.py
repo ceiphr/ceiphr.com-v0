17244f8087fbe844b504ff7103b048b8d2bffe89
Ari <ceiphr@pm.me>
2019-04-20 15:57:33 -0400

    Added more details to rss and atom feeds

--- ceiphrcom/feeds.py
+++ ceiphrcom/feeds.py
@@ -2,13 +2,16 @@ from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Atom1Feed
 from django.urls import reverse
 from database.models import Article
+import datetime
 
 class RssSiteNewsFeed(Feed):
     title = "Ceiphr.com - All Articles"
     author_name = "Ari Birnbaum"
+    author_email = 'contact@ceiphr.com'
     feed_url = "/blog"
     link = "/blog"
     description = "Updates on changes and additions to Ceiphr.com."
+    feed_copyright = 'Copyright (c) 2016 - %s, Ari Birnbaum (Ceiphr)' % datetime.date.today().year
 
     def items(self):
         return Article.objects.exclude(published=False).order_by('-modified')[:5]
